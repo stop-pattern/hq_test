a5636dcfc985513172346d9eb9a5debd9a4cebf6
stop_pattern <stoppattern+github@gmail.com>
2022-12-27 02:43:51 +0900

    add bve demo

--- src/main.cpp
+++ src/main.cpp
@@ -6,32 +6,55 @@ PCA9555 ioex;
 using PCA95x5::Level::Level;
 using PCA95x5::Port::Port;
 
+void bids();
 void writeSpeed(uint8_t speed);
 const int del = 300;
 
 void setup() {
+    pinMode(0, INPUT);
     Serial.begin(115200);
-    delay(2000);
+    delay(1000);
 
     Wire.begin(22, 21);  // sda,scl v1.00はデフォルトと逆
     ioex.attach(Wire);
     ioex.polarity(PCA95x5::Polarity::ORIGINAL_ALL);
     ioex.direction(PCA95x5::Direction::OUT_ALL);
     ioex.write(PCA95x5::Level::H_ALL);
+    delay(1000);
 }
 
 void loop() {
-    delay(1000);
     for (size_t i = 0; i < 140 + 1; i++) {
         writeSpeed(i);
         Serial.println(i);
         delay(del);
     }
-    delay(1000);
+    for (size_t i = 0; i < 1000; i++) {
+        if (digitalRead(0)) bids();
+        delay(1);
+    }
     for (int i = 140; i > 0; i--) {
         writeSpeed(i);
         Serial.println(i);
         delay(del);
+    }
+    for (size_t i = 0; i < 1000; i++) {
+        if (digitalRead(0)) bids();
+        delay(1);
+    }
+}
+
+/* --- for bve trainsim through bids serial --- */
+
+void bids() {
+    while (digitalRead(0)) {
+        Serial.println("TRIE1");
+        auto res = Serial.readStringUntil('\n');
+        Serial.println("> " + res);
+        auto spd = res.substring(6).toInt();
+        Serial.println("set speed: " + String(spd));
+        writeSpeed(spd);
+        delay(100);
     }
 }
 
